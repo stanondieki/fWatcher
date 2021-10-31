695424f12453c4893af294193e150e2538c4ce4a
Nyandika Donald <ondiekistanley21@gmail.com>
2021-10-31 18:19:34 +0300

    file commit

---
+++ filewatcher/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace filewatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = @"D:\try\sample.txt";
+
+            List<string> lines = File.ReadAllLines(filepath).ToList();
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+            lines.Add("space");
+
+            File.WriteAllLines(filepath, lines);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
